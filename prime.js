01d21ed1d01555fe64074c894fa27f688c4daa3a
Hrithik Vishwakarma <hritik8008@gmail.com>
2022-09-22 18:16:57 +0530

    code fix by hrithik

--- prime.js
+++ prime.js
@@ -1,7 +1,7 @@
 function prime(num){
 let count=0;
-    for(let i=0;i<=num;i++){
-	if(num%i){
+    for(let i=1;i<=num;i++){
+	if(num%i==0){
 	  count++;
  	}
       }
@@ -10,8 +10,4 @@ let count=0;
  }
  return false;
 }
-let ans=prime(13);
-if(answer==true){
-  console.log("Prime)
- }else{
- console.log("Not Prime")
+ console.log(prime(13))
